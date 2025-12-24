097c319afd0a6372f24ea20018aa6073b2a34e54
ReallySoggySocks <havenkain@gmail.com>
2025-12-24 14:18:09 -0600

    Created function to convert text nodes into leaf nodes

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "plain"
@@ -26,3 +27,20 @@ class TextNode():
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(text_node.content)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.content)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.content)
+        case TextType.CODE:
+            return LeafNode("code", text_node.content)
+        case TextType.LINK:
+            return LeafNode("a", text_node.content, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.content})
+        case _:
+            raise ValueError("Invalid leaf node text type")
