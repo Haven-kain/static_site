39b4b83075b152abaa9f7044780011c786f0f82e
ReallySoggySocks <havenkain@gmail.com>
2025-12-24 20:40:57 -0600

    altered block to block type function to take into account multi line lists, and quotes, fixed code blocktype check

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -17,23 +17,32 @@ def markdown_to_blocks(markdown):
     return blocks
 
 def block_to_block_type(block):
-    block = block[0]
-    if len(block.split("# ")[0]) > 5:
-        return BlockType.PARAGRAPH
-    
-    if len(block.split("# ")) == 2:
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "##### ")):
         return BlockType.HEADING
     
-    if block.startswith("```") and block.endswith("```"):
+    if len(lines) > 1 and block.startswith("```") and block.endswith("```"):
         return BlockType.CODE
     
     if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
         return BlockType.QUOTE
     
     if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.UNORDERED_LIST
         return BlockType.UNORDERED_LIST
     
     if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
         return BlockType.ORDERED_LIST
     
     return BlockType.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -70,28 +70,31 @@ This is the same paragraph on a new line
         h1 = "# heading"
 
         text_block = markdown_to_blocks(text)
-        text_block_type = block_to_block_type(text_block)
+        text_block_type = block_to_block_type(text_block[0])
 
         other_text = markdown_to_blocks(other_text)
-        other_text = block_to_block_type(other_text)
+        other_text = block_to_block_type(other_text[0])
 
         h1_block = markdown_to_blocks(h1)
-        h1_type = block_to_block_type(h1_block)
+        h1_type = block_to_block_type(h1_block[0])
 
         self.assertEqual(text_block_type, BlockType.PARAGRAPH)
         self.assertEqual(other_text, BlockType.PARAGRAPH)
         self.assertEqual(h1_type, BlockType.HEADING)
 
     def test_code_block_type(self):
-        code = "```this is code```"
+        code = """
+        ```
+        this is code
+        ```
+        """
         not_code = "```this is not code"
 
         code = markdown_to_blocks(code)
-        code = block_to_block_type(code)
-        print(code)
+        code = block_to_block_type(code[0])
 
         not_code = markdown_to_blocks(not_code)
-        not_code = block_to_block_type(not_code)
+        not_code = block_to_block_type(not_code[0])
 
         self.assertEqual(code, BlockType.CODE)
         self.assertEqual(not_code, BlockType.PARAGRAPH)
