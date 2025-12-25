abbf6b8f5eaafb919bfa90f4edb1fb24ea9d6cad
ReallySoggySocks <havenkain@gmail.com>
2025-12-24 20:12:36 -0600

    created block type and block to block type functions along with tests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph",
+    HEADING = "heading",
+    CODE = "code",
+    QUOTE = "quote",
+    UNORDERED_LIST = "ul",
+    ORDERED_LIST = "ol",
+
 def markdown_to_blocks(markdown):
     blocks = markdown.strip()
     blocks = blocks.split("\n\n")
@@ -5,3 +15,25 @@ def markdown_to_blocks(markdown):
         if block == "":
             blocks.remove(block)
     return blocks
+
+def block_to_block_type(block):
+    block = block[0]
+    if len(block.split("# ")[0]) > 5:
+        return BlockType.PARAGRAPH
+    
+    if len(block.split("# ")) == 2:
+        return BlockType.HEADING
+    
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    
+    if block.startswith(">"):
+        return BlockType.QUOTE
+    
+    if block.startswith("- "):
+        return BlockType.UNORDERED_LIST
+    
+    if block.startswith("1. "):
+        return BlockType.ORDERED_LIST
+    
+    return BlockType.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, BlockType, block_to_block_type
 
 class TestMarkdownBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -63,6 +63,49 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_heading_block_type(self):
+        text = "#heading"
+        other_text = "####### heading"
+        h1 = "# heading"
+
+        text_block = markdown_to_blocks(text)
+        text_block_type = block_to_block_type(text_block)
+
+        other_text = markdown_to_blocks(other_text)
+        other_text = block_to_block_type(other_text)
+
+        h1_block = markdown_to_blocks(h1)
+        h1_type = block_to_block_type(h1_block)
+
+        self.assertEqual(text_block_type, BlockType.PARAGRAPH)
+        self.assertEqual(other_text, BlockType.PARAGRAPH)
+        self.assertEqual(h1_type, BlockType.HEADING)
+
+    def test_code_block_type(self):
+        code = "```this is code```"
+        not_code = "```this is not code"
+
+        code = markdown_to_blocks(code)
+        code = block_to_block_type(code)
+        print(code)
+
+        not_code = markdown_to_blocks(not_code)
+        not_code = block_to_block_type(not_code)
+
+        self.assertEqual(code, BlockType.CODE)
+        self.assertEqual(not_code, BlockType.PARAGRAPH)
+
+    def test_quote_block(self):
+        quote = ">This is a quote"
+        another_quote = "> This is another quote"
+        not_a_quote = "This is not a quote >"
+
+        quote = markdown_to_blocks(quote)
+        another_quote = markdown_to_blocks(another_quote)
+        not_a_quote = markdown_to_blocks(not_a_quote)
+
+
 
 if __name__ == "__main__":
     unittest.main()
