440562590d3f9c4818ed585a2fa7fdcd73c3ef08
ReallySoggySocks <havenkain@gmail.com>
2025-12-30 16:41:26 -0600

    created nested inline markdown parsing

--- src/splitting_functions.py
+++ src/splitting_functions.py
@@ -8,12 +8,38 @@ Functionality to add later:
 -Dynamic checking of delimiters
 """
 
-def split_nodes_delimiter(old_node, delimiter, text_type):
+def split_nested_nodes(old_node, delimiter, text_type):
     new_nodes = []
     for node in old_node:
-        if node.text_type != TextType.TEXT:
-            new_nodes.append(node)
-            continue
+        
+        if "`" in node.text:
+            return split_nodes_delimiter([node], delimiter, text_type)
+        
+        split_nodes = split_nodes_delimiter([node], delimiter, text_type)
+
+        for i in range(len(split_nodes)):
+            current_node = split_nodes[i]
+            starting_char = current_node.text[0]
+            ending_char = current_node.text[-1]
+
+            if starting_char != ending_char:
+                continue
+
+            match starting_char and ending_char:
+                case "*":
+                    node = split_nodes_delimiter([node], "*", TextType.BOLD)
+                    split_nodes[i] = TextNode(node[1], current_node.text_type)
+                case "_":
+                    node = split_nodes_delimiter([node], "_", TextType.ITALIC)
+                    split_nodes[i] = TextNode(node[1], current_node.text_type)
+                case _:
+                    continue
+        new_nodes.extend(split_nodes)
+    return new_nodes
+
+def split_nodes_delimiter(old_node, delimiter, text_type):    
+    new_nodes = []
+    for node in old_node:
 
         split_node = node.text.split(delimiter)
         if len(split_node) % 2 == 0:
--- src/test_splitting.py
+++ src/test_splitting.py
@@ -1,9 +1,9 @@
 import unittest
 
-from splitting_functions import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from splitting_functions import split_nodes_delimiter, split_nested_nodes, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 
-class TestSplittingFuncs(unittest.TestCase):
+class TestSplittingDelims(unittest.TestCase):
     def test_code(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
@@ -55,6 +55,75 @@ class TestSplittingFuncs(unittest.TestCase):
             split_nodes_delimiter([node4], "_", TextType.ITALIC)
             split_nodes_delimiter([node5], "*", TextType.BOLD)
             split_nodes_delimiter([node6], "*", TextType.BOLD)
+
+class TestSplittingNested(unittest.TestCase):
+    def test_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nested_nodes([node], "`", TextType.CODE)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ]
+        )
+
+    def test_nested_code(self):
+        node = TextNode("This is text with a *`code block`* word", TextType.TEXT)
+        node2 = TextNode("This is text with a _`code block`_ word", TextType.TEXT)
+        node3 = TextNode("This is text with a *_`code block`_* word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node, node2, node3], "`", TextType.CODE)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a *", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode("* word", TextType.TEXT),
+                TextNode("This is text with a _", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode("_ word", TextType.TEXT),
+                TextNode("This is text with a *_", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode("_* word", TextType.TEXT)
+            ]
+        )
+
+    def test_nested_italic(self):
+        node = TextNode("This is text with a _*nested*_ word", TextType.TEXT)
+        new_nodes = split_nested_nodes([node], "_",TextType.ITALIC)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode(TextNode("nested", TextType.BOLD), TextType.ITALIC),
+                TextNode(" word", TextType.TEXT),
+            ]
+            )
+        
+    def test_nested_bold(self):
+        node = TextNode("This is text with a *_nested_* word", TextType.TEXT)
+        new_nodes = split_nested_nodes([node], "*", TextType.BOLD)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode(TextNode("nested", TextType.ITALIC), TextType.BOLD),
+                TextNode(" word", TextType.TEXT)
+            ]
+        )
+
+    def test_unmatched_delim(self):
+        node = TextNode("This is text with a _*normal* word", TextType.TEXT)
+        new_nodes = split_nested_nodes([node], "*", TextType.BOLD)
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a _", TextType.TEXT),
+                TextNode("normal", TextType.BOLD),
+                TextNode(" word", TextType.TEXT)
+            ]
+        )
 
 class TestExtraction(unittest.TestCase):
     def test_extract_markdown_images(self):
